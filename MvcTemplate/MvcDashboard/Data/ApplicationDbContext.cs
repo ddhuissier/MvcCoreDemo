fa877c7ccb6b92dbc8f540978ca25104df836f39
ddhuissier <ddhuissier@gmail.com>
2021-09-27 09:04:10 +0200

    First implementation (Identity+Charts)

---
+++ MvcTemplate/MvcDashboard/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcTemplate.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcTemplate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDataProtectionKeyContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+    }
+}
---
+++ MvcTemplate/MvcDashboard/Startup.cs
@@ -0,0 +1,200 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcTemplate.Data;
+using MvcTemplate.Infrastructure;
+using MvcTemplate.Infrastructure.ApplicationUserClaims;
+using MvcTemplate.Infrastructure.AppSettingsModels;
+using MvcTemplate.Infrastructure.Startup;
+using MvcTemplate.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTemplate
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed
+                // for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+            });
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Default Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Default SignIn settings.
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                // Default User settings.
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "Mvc.DotNetCore.AppCookie";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                // You might want to only set the application cookies over a secure connection:
+                // options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.SlidingExpiration = true;
+            });
+
+
+            services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, options =>
+            {
+                options.AccessDeniedPath = "/access-denied";
+                options.LoginPath = "/login";
+                options.LogoutPath = "/logout";
+
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+            });
+
+            services.AddDataProtection()
+                .PersistKeysToDbContext<ApplicationDbContext>();
+
+            services.AddAntiforgery();
+
+            services.Configure<ScriptTags>(Configuration.GetSection(nameof(ScriptTags)));
+
+            services.AddControllersWithViews(options =>
+            {
+                options.Conventions.Add(
+                  new RouteTokenTransformerConvention(
+                      new SlugifyParameterTransformer()));
+
+                // Enable Antiforgery feature by default on all controller actions
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AddAreaPageRoute("Identity", "/Account/Register", "/register");
+                options.Conventions.AddAreaPageRoute("Identity", "/Account/Login", "/login");
+                options.Conventions.AddAreaPageRoute("Identity", "/Account/Logout", "/logout");
+                options.Conventions.AddAreaPageRoute("Identity", "/Account/ForgotPassword", "/forgot-password");
+            })
+               .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+               .AddSessionStateTempDataProvider();
+
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            // You probably want to use in-memory cache if not developing using docker-compose
+            // services.AddMemoryCache();
+            //services.AddDistributedRedisCache(action => { action.Configuration = Configuration["Redis:InstanceName"]; });
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromMinutes(60);
+                options.Cookie.Name = "Mvc.DotNetCore.DotNetCore.SessionCookie";
+                // You might want to only set the application cookies over a secure connection:
+                // options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+
+            // This adds a hosted service that, on application start-up, seeds the database with initial data.
+            // You can remove this if you want to prevent the seeding process or you can change the initial data
+            // to suit your needs in the IdentityDataSeeder class.
+            services.AddHostedService<DbSeederHostedService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseStatusCodePagesWithReExecute("/status-code", "?code={0}");
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
